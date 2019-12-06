f9893c7f22271e585e4507bf9203941d53ab3b2c
Marlon Paulo <marlon.paulo.silva@outlook.com>
2019-12-06 17:49:22 -0300

    server.js

--- src/server.js
+++ src/server.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use("/files", express.static(path.resolve(__dirname, "..", "uploads")));
 app.use(routes);
 
-const port = 3333 || 8080;
+const port = process.env.PORT || 3333 || 8080;
 app.listen(port, err => {
   if (!err) {
     console.log("rodando na porta: ", port);
